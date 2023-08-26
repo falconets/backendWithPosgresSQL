c3df3586e79a891affd8e140ca449185c8a31f42
Richard <43894138+Richard-Chimama@users.noreply.github.com>
2023-08-26 13:04:48 +0200

    ADD NEW DATABASE CONFIGARATION

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,8 +4,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const isProduction = process.env.NODE_ENV === 'production'
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-console.log(connectionString)
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
+
 
 const Pool = new Pg.Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
