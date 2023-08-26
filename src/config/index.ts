d6434ee08e2123f2a78913828736d80ac260517e
Richard <43894138+Richard-Chimama@users.noreply.github.com>
2023-08-26 12:46:48 +0200

    try to connect to the database

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,7 @@ dotenv.config()
 
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+console.log(connectionString)
 
 const Pool = new Pg.Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
